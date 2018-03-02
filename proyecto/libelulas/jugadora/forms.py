a531d5a893839c3cb48dd835712ecf8ad7f7352a
ale <a01208598@itesm.mx>
2018-03-02 15:35:28 -0600

    Arreglar upload de jugadoras y falta arreglar detalle al momento de bajar la  calidad de una imagen

--- proyecto/libelulas/jugadora/forms.py
+++ proyecto/libelulas/jugadora/forms.py
@@ -2,6 +2,12 @@ from django.forms import ModelForm
 from .models import Jugadora
 
 class jugadoraForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(jugadoraForm, self).__init__(*args, **kwargs)
+        self.fields['Imagen'].widget.attrs['class'] = 'inputfile'
+        self.fields['Nacimiento'].widget.input_type = 'date'
+
     class Meta:
         model = Jugadora
         fields = ('Nombre', 'Apellido', 'Nacimiento', 'Numero', 'Posicion', 'Notas', 'Imagen')
--- proyecto/libelulas/jugadora/views.py
+++ proyecto/libelulas/jugadora/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from .forms import jugadoraForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def agregar_jugadora(request):
     if request.method == "POST":
-        form = jugadoraForm(request.POST)
+        form = jugadoraForm(request.POST, request.FILES)
         if form.is_valid():
             jugadora = form.save()
             jugadora.save()
             print("hola")
-            return reverse('agregar_jugadora')
+            return HttpResponseRedirect(reverse('jugadora:agregar_jugadora'))
         else:
             print("no valido")
     else:
