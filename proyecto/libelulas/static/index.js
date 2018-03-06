ea39e3cda61e7a9cfe0895d8cd7f66e6e697d6d0
icecreamsoldier <mariana.9000@hotmail.com>
2018-03-06 11:38:39 -0600

    Agregar calidad

--- proyecto/libelulas/static/index.js
+++ proyecto/libelulas/static/index.js
@@ -1,4 +1,15 @@
 let loadFile = function(event) {
   let output = document.getElementById('upload');
   output.src = URL.createObjectURL(event.target.files[0]);
-} 
+}
+
+function hideMessage() {
+  setTimeout(() => {
+    document.getElementById('message').style.animationPlayState = 'running'
+  }, 1000);
+};
+
+
+document.getElementById('message')
+? hideMessage()
+: null;
