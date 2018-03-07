73ee170d638a6ab091da210f35a244c0e42649fa
ale <a01208598@itesm.mx>
2018-03-07 10:35:24 -0600

    cambio del save en el view de editar jugadora

--- proyecto/libelulas/jugadora/views.py
+++ proyecto/libelulas/jugadora/views.py
@@ -30,7 +30,7 @@ def editar_jugadora(request, jugadora_id):
     if request.method == "POST":
         form = jugadoraForm(request.POST, request.FILES, instance=instance)
         if form.is_valid():
-            jugadora = form.save()
+            jugadora = form
             jugadora.save()
             messages.success(request, 'Jugadora editada exitosamente')
             return HttpResponseRedirect(reverse('jugadora:agregar_jugadora'))
