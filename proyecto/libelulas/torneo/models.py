6da05df57a470d41b9c2ba7e6cbb9ca5646dc2c2
RamonRomeroTec <A01700318@itesm.mx>
2018-03-09 14:28:10 -0600

    Modelo Torneo

--- proyecto/libelulas/torneo/models.py
+++ proyecto/libelulas/torneo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+import datetime
+from equipo.models import Equipo
 
-# Create your models here.
+
+
+class Torneo(models.Model):
+    nombre=models.CharField(max_length=100, verbose_name="Nombre del Torneo")
+    categoria=models.IntegerField(verbose_name="Categoria", validators=[MinValueValidator(datetime.datetime.now().year )])
+    fechaInicio=models.DateField(verbose_name="Fecha Inicio")
+    anexo=models.FileField(upload_to='media/torneo', blank=True, null=True, verbose_name="Documento Anexo")
+    costo=models.IntegerField(verbose_name="Categoria", validators=[MinValueValidator()])
+    equipo=models.ManyToManyField(Equipo)
+
