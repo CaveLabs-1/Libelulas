beebf4fa6880ec395dccdadd50df594a03798332
Ale Lopez <a01208598@itesm.mx>
2018-03-16 10:50:26 -0600

    fixes: Detalle Jugadora Modal de Borrar

--- jugadora/urls.py
+++ jugadora/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('agregar_jugadora/<int:equipo_id>', views.agregar_jugadora, name='agregar_jugadora'),
     path('ver_jugadoras/', views.ver_jugadoras, name='ver_jugadoras'),
     path('eliminar/<int:pk>', views.eliminar_jugadora.as_view(), name='eliminar_jugadora'),
+    path('<int:pk>', views.detalle_jugadora, name='detalle_jugadora'),
 ]
