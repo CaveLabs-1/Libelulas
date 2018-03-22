c8f3ff58ba4cd1cf79039d31d2350862c816b790
icecreamsoldier <mariana.9000@hotmail.com>
2018-03-21 18:25:41 -0600

    Terminar vista landing

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,9 +1,15 @@
+let nav = document.getElementById('navbar');
+let mobileNav = document.getElementById('mobile-nav');
+console.log(mobileNav);
 window.addEventListener('scroll', function (e) {
-  let nav = document.getElementById('navbar');
   if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+    mobileNav.classList.add('colored');
+    mobileNav.classList.remove('transparent');
     nav.classList.add('colored');
     nav.classList.remove('transparent');
   } else {
+    mobileNav.classList.add('transparent');
+    mobileNav.classList.remove('colored');
     nav.classList.add('transparent');
     nav.classList.remove('colored');
   }
