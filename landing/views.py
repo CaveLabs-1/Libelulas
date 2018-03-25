a2fca66f1a13214a63a2e11bc8fc34b6e7954557
icecreamsoldier <mariana.9000@hotmail.com>
2018-03-25 15:22:01 -0600

    Ver detalle equipos

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from equipo.models import Equipo
 from jugadora.models import Jugadora
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def ver_organizadores(request):
@@ -10,7 +11,7 @@ def ver_equipos(request):
     equipos = Equipo.objects.all()
     return render(request, 'landing/lista_equipos.html', {'equipos': equipos})
 
-def detalle_equipo(request, equipo_id):
-    equipo = get_object_or_404(Equipo, pk=equipo_id)
-    jugadoras_equipo = Equipo.objects.get(id=equipo_id).jugadoras.all()
-    return (request, 'landing/detalle_equipo.html', {'equipo': equipo, 'jugadoras_equipo': jugadoras_equipo})
+def detalle_equipo(request, pk):
+    equipo = get_object_or_404(Equipo, pk=pk)
+    jugadoras_equipo = Equipo.objects.get(id=pk).jugadoras.all()
+    return render (request, 'landing/detalle_equipo.html', {'equipo': equipo, 'jugadoras_equipo': jugadoras_equipo})
