1bd5d2c33fb4f6655f427125db973f64eb1c012a
Marco Mancha <A01206194@itesm.mx>
2018-04-01 18:42:38 -0500

    Terminar imlementacion

--- torneo/views.py
+++ torneo/views.py
@@ -152,11 +152,15 @@ def editar_partido(request, id_partido):
     form = PartidoForm(instance=partido)
     if request.method == "POST":
         form = PartidoForm(request.POST,instance=partido)
+        objeto = form.save(commit=False)
         if form.is_valid():
-            partido = form
-            partido.save()
-            messages.success(request, 'Partido editado exitosamente')
-            return HttpResponseRedirect(reverse('torneo:editar_registro',kwargs={'id_torneo':partido.jornada.torneo.id}))
+            if partido.jornada.fecha_inicio <= objeto.fecha and partido.jornada.fecha_fin >= objeto.fecha:
+                objeto.save()
+                messages.success(request, 'Partido editado exitosamente')
+                return HttpResponseRedirect(reverse('torneo:editar_registro',kwargs={'id_torneo':partido.jornada.torneo.id}))
+            else:
+                form = PartidoForm(instance=partido)
+                messages.warning(request, 'Fecha incorrecta')
         else:
             messages.warning(request, 'Hubo un error en la forma')
     return render(request, 'torneo/editar_partido.html', {'form': form, 'partido':partido})
