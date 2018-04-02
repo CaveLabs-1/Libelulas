a164bf7cf25b7c72e4f2f510207742a31aedb14f
pacomonroy <pacomonroyu@hotmail.com>
2018-04-02 12:25:10 -0500

    Consultar torneos y convocatorias

--- landing/urls.py
+++ landing/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from .import views
+from .views import *
 
 app_name = 'landing'
 
 urlpatterns = [
     # path('', views.main_page, name='main_page'),
     path('', TemplateView.as_view(template_name='landing/main.html')),
+    path('landing_torneo/', views.verTorneos,name='verTorneos'),
+
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,13 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from torneo.models import Torneo
+
+
+# Create your views here.
+
+def verTorneos (request):
+    torneos= Torneo.objects.filter(activo=True)
+    return render(request,'landing/torneos.html',{'torneos':torneos})
