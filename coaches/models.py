aa6dde337bd4e252534c3d56c3ba899636422fb0
Marco Mancha <A01206194@itesm.mx>
2018-04-09 13:15:31 -0500

    Corregir modelo

--- coaches/models.py
+++ coaches/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from torneo.models import Torneo
 
 class PreRegistro(models.Model):
     Nombre = models.CharField(max_length=50, default='', verbose_name='Nombre')
     Correo = models.EmailField(max_length=50, blank=False, null=False, unique=True)
     Notas = models.CharField(max_length=50, default='', verbose_name='Nombre')
-    codigo = models.CharField(max_length=16)
+    codigo = models.CharField(max_length=16, null=True)
+    torneo = models.ForeignKey(Torneo, on_delete = models.CASCADE, null=True)
