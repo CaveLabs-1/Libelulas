7ac7ef8762f88bc70c0f2df20dbbc3643e857c23
Marco Mancha <A01206194@itesm.mx>
2018-04-11 11:16:57 -0500

    Terminar US37 y agregar filtros activo

--- coaches/views.py
+++ coaches/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .models import PreRegistro
 from torneo.models import *
+from equipo.models import *
 from equipo.forms import equipoForm
+from jugadora.forms import *
 
 def pre_registro(request, id_torneo):
     torneo = get_object_or_404(Torneo,id=id_torneo)
@@ -16,14 +18,29 @@ def pre_registro(request, id_torneo):
             registro.torneo = torneo
             registro.save()
             messages.success(request, 'Solicitud de pre-registro enviada')
-            return HttpResponseRedirect(reverse('coaches:pre_registro', kwargs={'id_torneo':id_torneo}))
+            return HttpResponseRedirect(reverse('coaches:registrar_jugadora', kwargs={'codigo':codigo,'id_equipo':registro.equipo.id}))
     else:
         form = PreForm()
     return render(request, 'coaches/pre_registro.html', {'form': form})
 
-def registrar_jugadora(request, codigo):
-
-    return
+def registrar_jugadora(request, codigo, id_equipo):
+    lista_equipo = get_object_or_404(Equipo,id=id_equipo)
+    if request.method == "POST":
+        form = jugadoraEquipoForm(request.POST, request.FILES)
+        if form.is_valid():
+            jugadora = form.save(commit=False)
+            jugadora.activo = False
+            jugadora.save()
+            e = Equipo.objects.get(id=id_equipo)
+            e.jugadoras.add(jugadora)
+            e.save()
+            messages.success(request, 'Jugadora agregada exitosamente')
+            return HttpResponseRedirect(reverse('coaches:registrar_jugadora', kwargs={'codigo':codigo,'id_equipo':id_equipo}))
+        else:
+            messages.warning(request, 'Hubo un error en la forma')
+    else:
+        form = jugadoraEquipoForm()
+    return render(request, 'coaches/agregar_jugadora.html', {'form': form, 'lista_equipo':lista_equipo, 'equipo':id_equipo, 'codigo':codigo})
 
 def registrar_equipo(request, codigo):
     registro = get_object_or_404(PreRegistro,codigo=codigo)
@@ -41,4 +58,11 @@ def registrar_equipo(request, codigo):
             messages.warning(request, 'Hubo un error en la forma')
     else:
             form = equipoForm()
-    return render(request, 'equipo/agregar_equipo.html', {'form': form})
+    return render(request, 'coaches/agregar_equipo.html', {'form': form})
+
+def terminar_registro(request, codigo):
+    registro = get_object_or_404(PreRegistro,codigo=codigo)
+    registro.codigo = ""
+    registro.save()
+    messages.success(request, 'La solicitud será procesada por el administrador')
+    return HttpResponseRedirect(reverse('landing:ver_equipos'))
