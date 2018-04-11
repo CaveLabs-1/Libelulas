c50734692f20be066d25d45452203c93414d7171
icecreamsoldier <mariana.9000@hotmail.com>
2018-04-10 19:53:15 -0500

    Terminar Diseno

--- static/js/landing.js
+++ static/js/landing.js
@@ -14,3 +14,67 @@ window.addEventListener('scroll', function (e) {
     nav.classList.remove('colored');
   }
 });
+
+let partidosJugados = document.getElementById('partidos');
+let golesAnotados = document.getElementById('golesAnotados');
+if (partidosJugados && golesAnotados) {
+  partidosJugados = partidosJugados.getAttribute('data-value');
+  partidosJugadosE = partidosJugados.slice(0, partidosJugados.indexOf(' '));
+  partidosJugadosJ = partidosJugados.slice(partidosJugados.lastIndexOf(' ')+1);
+  golesAnotados = golesAnotados.getAttribute('data-value');
+  golesAnotadosE = golesAnotados.slice(0, golesAnotados.indexOf(' '));
+  golesAnotadosJ = golesAnotados.slice(golesAnotados.lastIndexOf(' ')+1);
+  window.onload = function onLoad() {
+    var bar = new ProgressBar.Circle('#partidos', {
+      color: '#009CDE',
+      strokeWidth: 4,
+      trailWidth: 1,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+        autoStyleContainer: false
+      },
+      from: { color: '#009CDE', width: 4 },
+      to: { color: '#009CDE', width: 4 },
+      step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = partidosJugadosJ;
+        if (value === 0) {
+          circle.setText('');
+        } else {
+          circle.setText(value);
+        }
+
+      }
+    });
+    bar.animate(partidosJugadosE);
+    bar.text.style.color = 'rgba(3,27,78,.7)';
+    var bar2 = new ProgressBar.Circle('#golesAnotados', {
+      color: '#009CDE',
+      strokeWidth: 4,
+      trailWidth: 1,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+        autoStyleContainer: false
+      },
+      from: { color: '#009CDE', width: 4 },
+      to: { color: '#009CDE', width: 4 },
+      step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = golesAnotadosJ;
+        if (value === 0) {
+          circle.setText('');
+        } else {
+          circle.setText(value);
+        }
+      }
+    });
+    bar2.animate(golesAnotadosE);
+    bar2.text.style.color = 'rgba(3,27,78,.7)';
+  };
+}
