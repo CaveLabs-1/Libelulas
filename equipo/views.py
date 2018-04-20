be49cd233ebc2a6e838f95db9291832f6dfc8621
Ale Lopez <a01208598@itesm.mx>
2018-04-20 13:45:46 -0500

    mas refactoring

--- equipo/views.py
+++ equipo/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
 from django.urls import reverse
-
-# Create your views here.
 from .forms import equipoForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.views.generic.list import ListView
+from .models import Equipo
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+from jugadora.models import Jugadora
 
 # Create your views here.
 
-from django.views.generic.list import ListView
-from .models import Equipo
-
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import DeleteView
-
-
-from django.urls import reverse_lazy
-from jugadora.models import Jugadora
 
 def agregar_equipo(request):
     if request.method == "POST":
@@ -37,13 +32,14 @@ def agregar_equipo(request):
 class lista_equipos(ListView):
     model = Equipo
     queryset = Equipo.objects.filter(activo=True)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
 
 
 def detalle_equipo(request, equipo_id):
-    equipo = get_object_or_404(Equipo ,pk=equipo_id)
+    equipo = get_object_or_404(Equipo, pk=equipo_id)
     jugadoras_equipo = Equipo.objects.get(id=equipo_id).jugadoras.filter(activo=True)
     return render(request, 'equipo/equipo_detail.html', {'equipo': equipo, 'jugadoras_equipo': jugadoras_equipo })
 
@@ -61,18 +57,22 @@ def editar_equipo(request, pk):
             messages.warning(request, 'Hubo un error en la forma')
     return render(request, 'equipo/equipo_update.html', {'form': form, 'equipo' : instance})
 
+
 class borrar_equipo(DeleteView):
     model = Equipo
+
     def delete(self, request, *args, **kwargs):
         messages.success(request, 'Equipo eliminado exitosamente')
         return super(borrar_equipo, self).delete(request, *args, **kwargs)
     success_url = reverse_lazy('equipo:lista_equipos')
+
 
 def eliminar_jugadora(request, equipo_id, idJugadora):
     j = Jugadora.objects.get(id=idJugadora)
     Equipo.objects.get(id=equipo_id).jugadoras.remove(j)
     messages.warning(request, 'Jugadora eliminada exitosamente del equipo')
     return HttpResponseRedirect(reverse_lazy('equipo:lista_equipos'))
+
 
 def welcome(request):
     return render(request, 'equipo/welcome.html')
